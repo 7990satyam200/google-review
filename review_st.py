8b8acafdd7f71502adf08f0982270f9d827b5d49
rcl1 <satyam.mishra@redcliffelabs.com>
2022-07-25 13:59:31 +0530

    update

--- review_st.py
+++ review_st.py
@@ -18,10 +18,12 @@ service_provider = st.sidebar.selectbox(
     "Select Diagnostic Service Provider",
     ("Redcliffe Labs", "Healthians", "Lal PathLabs")
 )
-v_polarity = st.sidebar.checkbox('Show Review Polarity/Rating Plots ', False)
-my_slider = st.sidebar.checkbox('Select Reviews with Polarity Values', False)
-wordcloud = st.sidebar.checkbox('Visualize WordCloud', False)
-grams = st.sidebar.checkbox('Visualize Unigrams, Bigrams and Trigrams', False)
+
+type_modeling = st.sidebar.selectbox(
+    "Select Reviews Type",
+    ("All", "Positive Reviews", "Negative Reviews")
+)
+
 topic_distribution = st.sidebar.checkbox('Topic distribution', True)
 
 # nlp = spacy.load("en_core_web_sm")
@@ -42,12 +44,23 @@ def read_data():
     redcliffe_labs['review_text'] = pd.DataFrame(redcliffe_labs.review_text.apply(lambda x: clean_text(x)))
     # redcliffe_labs["review_text"] = redcliffe_labs.apply(lambda x: lemmatizer(x['review_text']), axis=1)
     redcliffe_labs['review_lemmatize_clean'] = redcliffe_labs['review_text'].str.replace('-PRON-', '')
-    redcliffe_labs['polarity'] = redcliffe_labs.review_lemmatize_clean.apply(detect_polarity)
-
     return redcliffe_labs
 
 
+#
+# @st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
+# def get_review_type():
 redcliffe_labs = read_data()
+if type_modeling == "Negative Reviews":
+    redcliffe_labs = redcliffe_labs[redcliffe_labs.review_rating.isin([1, 2])]
+elif type_modeling == "Positive Reviews":
+    redcliffe_labs = redcliffe_labs[redcliffe_labs.review_rating.isin([4, 5])]
+    # return redcliffe_labs
+
+# redcliffe_labs = get_review_type()
+
+with st.expander('See data:'):
+    st.write(redcliffe_labs[['review_text', 'review_rating']].head(20))
 
 st.title(service_provider + ' Google Reviews')
 
@@ -92,142 +105,9 @@ components.v1.html(html_string, width=1000, height=800, scrolling=True)
 with st.expander("See Data"):
     st.write(redcliffe_labs.head())
 
-if my_slider:
-    st.subheader('Select Reviews with Polarity Values')
-    with st.form(key='my_form'):
-        values = st.slider(
-            'Select a range of Review Polarity values',
-            -1.0, 1.0, (-1.0, 0.51))
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            st.write('Selected Polarity Values:', values)
-            st.write(redcliffe_labs[['polarity', 'review_text', 'review_rating']][
-                         redcliffe_labs['polarity'].between(values[0], values[1])])
-        else:
-            st.write(redcliffe_labs[['polarity', 'review_text', 'review_rating']][
-                         redcliffe_labs['polarity'].between(-1.0, 0.51)])
-
-if v_polarity:
-    st.subheader('Polarity Values Plots ')
-    # A histogram of the polarity scores.
-    num_bins = 50
-    fig_pol = plt.figure(figsize=(10, 6))
-    n, bins, patches = plt.hist(redcliffe_labs.polarity, num_bins, facecolor='blue', alpha=0.5)
-    plt.xlabel('Polarity')
-    plt.ylabel('Count')
-    # plt.title('Histogram of polarity')
-    st.pyplot(fig_pol)
-
-    fig_rat = plt.figure(figsize=(10, 6))
-    sns.boxenplot(x='review_rating', y='polarity', data=redcliffe_labs)
-    st.pyplot(fig_rat)
-
-if wordcloud:
-    st.subheader('WordCloud (cluster, of all reviews:)')
-    st.info(
-        'The more a specific word appears in a source of reviews, the bigger and bolder it appears in the word cloud.')
-
-    mpl.rcParams['figure.figsize'] = (12.0, 12.0)
-    mpl.rcParams['font.size'] = 12
-    mpl.rcParams['savefig.dpi'] = 100
-    mpl.rcParams['figure.subplot.bottom'] = .1
-    stopwords = set(STOPWORDS)
-
-    wordcloud = WordCloud(
-        background_color='white',
-        stopwords=stopwords,
-        max_words=500,
-        max_font_size=40,
-        random_state=42
-    ).generate(str(redcliffe_labs['review_text']))
-
-    print(wordcloud)
-    fig_21 = plt.figure(1)
-    plt.imshow(wordcloud)
-    plt.axis('off')
-    st.pyplot(fig_21)
-    # wordcloud = WordCloud(
-    #     background_color='white',
-    #     stopwords=stopwords,
-    #     max_words=500,
-    #     max_font_size=40,
-    #     random_state=42
-    # ).generate(str(redcliffe_labs['review_text'][]))
-    #
-    # print(wordcloud)
-    # fig = plt.figure(1)
-    # plt.imshow(wordcloud)
-    # plt.axis('off')
-    # st.pyplot(fig)
-
-if grams:
-    st.subheader('Most Frequently occurring sequence of N words')
-    common_words = get_top_n_words(redcliffe_labs['review_lemmatize_clean'], 30)
-    uni_gram = pd.DataFrame(common_words, columns=['unigram', 'count'])
-
-    fig = go.Figure([go.Bar(x=uni_gram['unigram'], y=uni_gram['count'])])
-    fig.update_layout(
-        title=go.layout.Title(text="Top 30 unigrams in the reviews."))
-    st.plotly_chart(fig, use_container_width=True)
-
-    common_words = get_top_n_bigram(redcliffe_labs['review_lemmatize_clean'], 20)
-    bi_gram = pd.DataFrame(common_words, columns=['bigram', 'count'])
-    fig = go.Figure([go.Bar(x=bi_gram['bigram'], y=bi_gram['count'])])
-    fig.update_layout(
-        title=go.layout.Title(text="Top 20 bigrams in the reviews."))
-    st.plotly_chart(fig, use_container_width=True)
-
-    common_words = get_top_n_trigram(redcliffe_labs['review_lemmatize_clean'], 20)
-    tri_gram = pd.DataFrame(common_words, columns=['trigram', 'count'])
-    fig = go.Figure([go.Bar(x=tri_gram['trigram'], y=tri_gram['count'])])
-    fig.update_layout(title=go.layout.Title(text="Top 20 trigrams in the reviews"))
-    st.plotly_chart(fig, use_container_width=True)
-# st.write(uni_gram)
-# st.write(bi_gram)
-# st.write(tri_gram)
-
-
-with st.expander("Reviews that have the Highest polarity or Lowest Polarity:"):
-    st.write("Reviews that have the Highest polarity:")
-    st.write(redcliffe_labs[redcliffe_labs.polarity == 1].review_text.head(25))
-    st.write("Reviews that have the lowest polarity:")
-    st.write(redcliffe_labs[redcliffe_labs.polarity == -1].review_text.head(25))
-    st.write("Reviews that have the lowest ratings:")
-    st.write(redcliffe_labs[redcliffe_labs.review_rating == 1].review_text.head(25))
-    # st.write("Reviews that have the Highest ratings:")
-    # st.write(redcliffe_labs[redcliffe_labs.review_rating == 5].review_text.head())
-    # st.write("Reviews that have lowest polarity (most negative sentiment) but with a 5-star:")
-    # st.write(redcliffe_labs[(redcliffe_labs.review_rating == 5) & (redcliffe_labs.polarity == -1)].head(10))
-    # st.write("Reviews that have the highest polarity (most positive sentiment) but with a 1-star:")
-    # st.write(redcliffe_labs[(redcliffe_labs.review_rating == 1) & (redcliffe_labs.polarity == 1)].head(10))
-
-    # st.write(redcliffe_labs.head())
-
-with st.expander("See Distribution of review character length"):
-    plt.figure(figsize=(10, 6))
-    doc_lens = [len(d) for d in redcliffe_labs.review_text]
-    fig, ax = plt.subplots()
-    plt.hist(doc_lens, bins=100)
-    plt.title('Distribution of review character length')
-    plt.ylabel('Number of reviews')
-    plt.xlabel('Review character length')
-    sns.despine()
-    st.pyplot(fig)
-
 if topic_distribution:
     st.title(service_provider + ' Topic Distribution')
-    # with st.form(key='Words_form'):
-    #     words = int(st.text_input(
-    #         "Number of Words",
-    #         10
-    #     ))
-    #     submitted = st.form_submit_button("Submit")
-    #     if submitted:
-    #         words = words
-    #     else:
-    #         words = 10
 
-    # Topic - Keywords Dataframe
     df_topic_keywords = pd.DataFrame(topic_keywords)
     df_topic_keywords.columns = ['Word ' + str(i) for i in range(df_topic_keywords.shape[1])]
     topics = ['Topic ' + str(i) for i in range(df_topic_keywords.shape[0])]
